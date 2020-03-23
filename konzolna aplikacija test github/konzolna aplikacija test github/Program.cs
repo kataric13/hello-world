da5bdb0af037ae85271623d6414f83e0d9dbe247
Tamara Kataric <tkataric@gmail.com>
2020-03-23 14:52:27 +0100

    Prve tri linije koda. Komplikovan nacin da se napise hello world

---
+++ konzolna aplikacija test github/konzolna aplikacija test github/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konzolna_aplikacija_test_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            string m = "world";
+            Console.WriteLine(s + " " + m) ;
+            Console.ReadKey();
+        }
+    }
+}
